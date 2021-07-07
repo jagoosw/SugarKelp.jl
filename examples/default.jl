a1470f207cf51c657d33b37c34886ea8243cbc1f
Jago <jagoosw@protonmail.com>
2021-07-07 20:43:01 +0100

    changed labels on default

--- examples/default.jl
+++ examples/default.jl
@@ -2,23 +2,21 @@ using Plots, Dates, Measures
 
 include("../src/Kelp.jl")
 
-t_i = 8.5 * 30
-nd = 5 * 365
-lat = 45
-u = 0.15
+t_i=8.0*30
+nd=365
+lat=60.25
+u=0.15
 
 #generate the time series as done in paper
 u_arr, temp, irr, ex_n = Kelp.defaults(t_i, t_i + nd, u)
 
-a_0 = 0.56;n_0 = 0.018;c_0 = 0.25
+a_0 = 30;n_0 = 0.009;c_0 = 0.3
 
 #solve
 solution, results = Kelp.solvekelp(t_i, nd, u_arr, temp, irr, ex_n, lat, a_0, n_0, c_0)
 
 #plots
 pyplot()
-
-display(display(plot(layout=grid(2, 3))))
 
 temp_disp=[];irr_disp=[];n_disp=[];
 
@@ -29,20 +27,22 @@ for t in solution.t
 end
 
 t_disp=solution.t
-t_lim=(t_i+nd-365,t_i+nd)
+t_lim=(t_i,t_i+365)
 
-plot!(t_disp,temp_disp,sp=1,ylabel="Temperature/degC",xlim=t_lim)
-plot!(t_disp,irr_disp,sp=2,ylabel="Irradiance/micro mol photons / m^2 / s",xlim=t_lim)
-plot!(t_disp,n_disp,sp=3,ylabel="Nitrate/micro mol / L",xlim=t_lim)
-plot!(t_disp,results.area,sp=4,xlabel="Month", ylabel="Frond Area/dm^2",xlim=t_lim,ylim=(35,45))
-plot!(t_disp,results.nitrogen,sp=5, xlabel="Month", ylabel="Nitrogen reserve/gN/g sw",xlim=t_lim)
-plot!(t_disp,results.carbon,sp=6, xlabel="Month", ylabel="Carbon reserve/gC/g sw",xlim=t_lim)
+display(display(plot(layout=grid(2, 3))))
+
+plot!(t_disp,temp_disp,sp=1,ylabel="Temperature/degC",xlim=t_lim,label="Analytical inputs")
+plot!(t_disp,irr_disp,sp=2,ylabel="Irradiance/micro mol photons / m^2 / s",xlim=t_lim,label="Analytical inputs")
+plot!(t_disp,n_disp,sp=3,ylabel="Nitrate/micro mol / L",xlim=t_lim,label="Analytical inputs")
+plot!(t_disp,results.area,sp=4,xlabel="Month", ylabel="Frond Area/dm^2",xlim=t_lim,ylim=(30,45),label="Analytical inputs")
+plot!(t_disp,results.nitrogen,sp=5, xlabel="Month", ylabel="Nitrogen reserve/gN/g sw",xlim=t_lim,label="Analytical inputs")
+plot!(t_disp,results.carbon,sp=6, xlabel="Month", ylabel="Carbon reserve/gC/g sw",xlim=t_lim,label="Analytical inputs")
 
 xticks=[]
-for day in t_i+nd-365:30:nd+t_i
+for day in t_i:30:t_i+365
     push!(xticks,Dates.format((Date(2015,1,1)+Dates.Day(day)),"U")[1])
 end
 
-plot!(xticks=([t_i+nd-365:30:nd+t_i;],xticks),legend=false,margin=-3mm)
+plot!(xticks=([t_i:30:t_i+365;],xticks),margin=-3mm)#legend=false)
 
 display(display(plot!()))
